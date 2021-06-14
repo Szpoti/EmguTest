2e1fcfb716d22b493b80c8e89c9f95621670dfc2
Szpoti <balage.farago@gmail.com>
2021-06-14 12:52:26 +0200

    remove catch

--- Program.cs
+++ Program.cs
@@ -7,48 +7,32 @@ namespace ConsoleApp1
     class Program
     {
         static QRCodeDetector detector;
-        static VideoCapture videoCapture = new();
+        static VideoCapture videoCapture = new VideoCapture();
         private static ManualResetEvent waitHandle = new ManualResetEvent(false);
 
         static void Main(string[] args)
         {
             Console.WriteLine("Started!");
-            Console.WriteLine("Loading files");
-            Console.WriteLine("videoCapture.ImageGrabbed += Capture_ImageGrabbed;");
             videoCapture.ImageGrabbed += Capture_ImageGrabbed;
-            Console.WriteLine("detector = new();");
             detector = new();
-            Console.WriteLine("videoCapture.Start();");
             videoCapture.Start();
-            Console.WriteLine("waitHandle.WaitOne();");
             waitHandle.WaitOne();
         }
 
         private static void Capture_ImageGrabbed(object sender, EventArgs e)
         {
             Console.WriteLine("capturing");
-            try
+            Mat m = new Mat();
+            Mat o = new Mat();
+            videoCapture.Retrieve(m);
+            var qrs = detector.Detect(m, o);
+            if (qrs)
             {
-                Mat m = new();
-                Mat o = new();
-                videoCapture.Retrieve(m);
-                var qrs = detector.Detect(m, o);
-                if (qrs)
+                string code = detector.Decode(m, o, new Mat());
+                if (code != "")
                 {
-                    string code = detector.Decode(m, o, new Mat());
-                    if (code != "")
-                    {
-                        Console.WriteLine(code);
-                    }
+                    Console.WriteLine(code);
                 }
-            }
-            catch (Exception ex)
-            {
-                videoCapture.ImageGrabbed -= Capture_ImageGrabbed;
-                videoCapture.Stop();
-                videoCapture = null;
-                Console.WriteLine(ex.Message);
-                throw;
             }
         }
     }
